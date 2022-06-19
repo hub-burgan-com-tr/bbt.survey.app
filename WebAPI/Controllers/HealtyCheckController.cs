c3e7d2d8b44b8580f14d3cbe5ffa1fb450c39f17
yarasaa <makbaba@burgan.com.tr>
2022-06-20 00:06:59 +0300

    update

---
+++ WebAPI/Controllers/HealtyCheckController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealtyCheckController : ControllerBase
+    {
+        [HttpGet]
+
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                message = true
+            });
+        }
+    }
+}
