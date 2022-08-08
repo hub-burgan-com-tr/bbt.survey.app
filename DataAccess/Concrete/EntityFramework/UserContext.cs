ea83907546adccc4aec2f579f8013d5a5b1d1d30
Mehmet Akbaba <42210846+yarasaa@users.noreply.github.com>
2022-08-08 17:32:01 +0300

    Update UserContext.cs

--- DataAccess/Concrete/EntityFramework/UserContext.cs
+++ DataAccess/Concrete/EntityFramework/UserContext.cs
@@ -13,10 +13,7 @@ namespace DataAccess.Concrete.EntityFramework
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            //optionsBuilder.UseNpgsql(@"Host=localhost;Database=BurganUser;Username=postgres;Password=lamal159***");
-            //optionsBuilder.UseNpgsql(@"Host=18.192.189.47:5433;Database=yugabyte;Username=admin;Password=rd2OZvpjbrthkDc0ASgiteXnUKCX6Y");
-            //optionsBuilder.UseSqlServer(@"Server=DESKTOP-0UBP1P5;Database=SurveyTest;Trusted_Connection=true");
-            //optionsBuilder.UseSqlServer(@"Server=10.180.20.125;Database=SurveyTest;User Id=DEBUGUSER;Password=aAR=GsG4");
+            
             IConfigurationRoot configuration = new ConfigurationBuilder()
             .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
             .AddJsonFile("appsettings.Test.json",optional:false,reloadOnChange:true)
